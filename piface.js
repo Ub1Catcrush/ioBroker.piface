8949846911b5c263b2db7a39fea5d191f5cc9768
Lars Weimar <Eisbaeeer@gmail.com>
2016-04-25 14:13:10 +0200

    adding set objects

--- piface.js
+++ piface.js
@@ -1,122 +1,152 @@
-/* jshint -W097 */// jshint strict:false
-/*jslint node: true */
-"use strict";
+/**
+ *
+ *      ioBroker PiFace Adapter
+ *
+ *      Copyright (c) 2016 Eisbaeeer@gmail.com
+ *
+ *      MIT License
+ *
+ */
 
-// you have to require the adapter module and pass a options object
-var utils = require(__dirname + '/lib/utils'); // Get common adapter utils
+var utils =   require(__dirname + '/lib/utils'); // Get common adapter utils
+var adapter = utils.adapter('piface');
+var PiFaceIN = [];
+var inp = [];
 
-var objects = {};
+adapter.on('ready', function () {
+    main();
+});
 
-var adapter = utils.adapter({    // name has to be set and has to be equal to adapters folder name and main file name excluding extension
-    name:  'piface',
-    
-    // is called if a subscribed state changes
-    stateChange: function (id, state) {
-        adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+adapter.on('objectChange', function (id, obj) {
+    adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+});
 
-        if (!state.ack) {
-            var ids = id.split(".");
-            ids = ids[ids.length - 1];
-                
-         adapter.log.info('ids ' + ids);     
-       
-        }
-    },
+adapter.on('stateChange', function (id, state) {
+    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
 
-        
-  
-    // is called when adapter shuts down - callback has to be called under any circumstances!
-    unload: function (callback) {
-        try {
-            eiscp.close();
-        } finally {
-            callback();
-        }
-    },
+    // you can use the ack flag to detect if state is command(false) or status(true)
+    if (!state.ack) {
+        adapter.log.info('ack is not set!');
+    }
+});
 
-    ready: function () {
-        adapter.subscribeStates('*');
-        main();
-    }  
-  
-  });
-  
+
 function pifaceinit(){
+    adapter.log.info('function pifaceinit() called!');
+}
+
+function pifaceread() {
     var PIFD = require('node-pifacedigital');
     var pi = new PIFD.PIFaceDigital(0,true);
-}  
-  
-function getinputs() {
-        var val = pi.getInput();
-      adapter.log.info('Inputs: ' + val);
+    
+        PiFaceIN[0] = pi.get(0);
+        PiFaceIN[1] = pi.get(1);
+        PiFaceIN[2] = pi.get(2);
+        PiFaceIN[3] = pi.get(3);
+        PiFaceIN[4] = pi.get(4);
+        PiFaceIN[5] = pi.get(5);
+        PiFaceIN[6] = pi.get(6);
+        PiFaceIN[7] = pi.get(7);
+        
+        // change to "true"|"false"
+                if (PiFaceIN[0] == '1') {
+          PiFaceIN[0] = true
+          }
+        if (PiFaceIN[0] == '0') {
+          PiFaceIN[0] = false
+          }
+
+        if (PiFaceIN[1] == '1') {
+          PiFaceIN[1] = true
+          }
+        if (PiFaceIN[1] == '0') {
+          PiFaceIN[1] = false
+          }
+
+        if (PiFaceIN[2] == '1') {
+          PiFaceIN[2] = true
+          }
+        if (PiFaceIN[2] == '0') {
+          PiFaceIN[2] = false
+          }
+
+        if (PiFaceIN[3] == '1') {
+          PiFaceIN[3] = true
+          }
+        if (PiFaceIN[3] == '0') {
+          PiFaceIN[3] = false
+          }
+
+        if (PiFaceIN[4] == '1') {
+          PiFaceIN[4] = true
+          }
+        if (PiFaceIN[4] == '0') {
+          PiFaceIN[4] = false
+          }
+
+        if (PiFaceIN[5] == '1') {
+          PiFaceIN[5] = true
+          }
+        if (PiFaceIN[5] == '0') {
+          PiFaceIN[5] = false
+          }
+
+        if (PiFaceIN[6] == '1') {
+          PiFaceIN[6] = true
+          }
+        if (PiFaceIN[6] == '0') {
+          PiFaceIN[6] = false
+          }
+
+        if (PiFaceIN[7] == '1') {
+          PiFaceIN[7] = true
+          }
+        if (PiFaceIN[7] == '0') {
+          PiFaceIN[7] = false
+          }
+
+// set objects if input is changed
+            if (PiFaceIN[0] !== inp[0]) {
+                adapter.setState (adapter.namespace + '.' + 'IN0', {val: PiFaceIN[0], ack: true});
+                inp[0] = PiFaceIN[0];
+            }
+            if (PiFaceIN[1] !== inp[1]) {
+                adapter.setState (adapter.namespace + '.' + 'IN1', {val: PiFaceIN[1], ack: true});
+                inp[1] = PiFaceIN[1];
+            }
+            if (PiFaceIN[2] !== inp[2]) {
+                adapter.setState (adapter.namespace + '.' + 'IN2', {val: PiFaceIN[2], ack: true});
+                inp[2] = PiFaceIN[2];
+            }
+            if (PiFaceIN[3] !== inp[3]) {
+                adapter.setState (adapter.namespace + '.' + 'IN3', {val: PiFaceIN[3], ack: true});
+                inp[3] = PiFaceIN[3];
+            }
+            if (PiFaceIN[4] !== inp[4]) {
+                adapter.setState (adapter.namespace + '.' + 'IN4', {val: PiFaceIN[4], ack: true});
+                inp[4] = PiFaceIN[4];
+            }
+            if (PiFaceIN[5] !== inp[5]) {
+                adapter.setState (adapter.namespace + '.' + 'IN5', {val: PiFaceIN[5], ack: true});
+                inp[5] = PiFaceIN[5];
+            }
+            if (PiFaceIN[6] !== inp[6]) {
+                adapter.setState (adapter.namespace + '.' + 'IN6', {val: PiFaceIN[6], ack: true});
+                inp[6] = PiFaceIN[6];
+            }
+            if (PiFaceIN[7] !== inp[7]) {
+                adapter.setState (adapter.namespace + '.' + 'IN7', {val: PiFaceIN[7], ack: true});
+                inp[7] = PiFaceIN[7];
+            }
+
+    adapter.log.info('Inputs: ' + PiFaceIN[0] + ":" + PiFaceIN[1] + ":" + PiFaceIN[2] + ":" + PiFaceIN[3] + ":" + PiFaceIN[4] + ":" + PiFaceIN[5] + ":" + PiFaceIN[6] + ":" + PiFaceIN[7] );
 }
-
-function decimalToHex(d, padding) {
-    var hex = Number(d).toString(16);
-    padding = typeof (padding) === "undefined" || padding === null ? padding = 2 : padding;
-
-    while (hex.length < padding) {
-        hex = "0" + hex;
-    }
-
-    return hex;
-}
-
-function createObjects () {
-      // Datenpunkte anlegen
-      var role = 'button';
-      var value = '';
-      var datapoints = new Array(
-          'OUT0',
-          'OUT1',
-          'OUT2',
-          'OUT3',
-          'OUT4',
-          'OUT5',
-          'OUT6',
-          'OUT7',
-          'IN0',
-          'IN1',
-          'IN2',
-          'IN3',
-          'IN4',
-          'IN5',
-          'IN6',
-          'IN7'
-          );
-      
-      for ( var i=0 ; i < datapoints.length ; i++ )  {
-          adapter.log.info('My array objects: ' + adapter.namespace + '.' + datapoints[i] + ', role = ' + role);        
-
-      // Create DP command if not exist and config fixedvar active
-            
-      adapter.log.info('Create new object: ' + adapter.namespace + '.' + datapoints[i] + ', role = ' + role);
-      
-        objects[adapter.namespace + '.' + datapoints[i]] = {
-            _id: adapter.namespace + '.' + datapoints[i],
-            common: {
-                name: datapoints[i],
-                role: role,
-                type: 'number'
-            },
-            native: {
-                command: datapoints[i]
-            },
-            type: 'state'
-        };
-
-        adapter.setObject(datapoints[i], objects[adapter.namespace + '.' + datapoints[i]], function (err, obj) {
-            adapter.setState(datapoints[i], {val: value, ack: true});
-        });
-    }
-   }
 
 function main() {
-    // First create the objects
-     createObjects();
-     pifaceinit();
-     getinputs();
-        
+    pifaceinit();
+    setInterval(pifaceread, 10000);
+//    setInterval(pifaceread, adapter.config.interval * 1000);
+    adapter.subscribeStates('*');
+            adapter.log.info('function main() called!');
 }
-
 
