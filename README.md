c5615ceafa819cd809d9c425056c26f9a81cd849
Lars Weimar <Eisbaeeer@gmail.com>
2016-04-25 22:28:55 +0200

    ### 0.0.20 first beta
    First beta for testing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![Logo](admin/piface.png)
+f![Logo](admin/piface.png)
 # ioBroker.piface
 
 This adapter allows to control a Piface on Raspberry Pi .
@@ -9,10 +9,34 @@ The adapter creates 8 input and output objects in iobroker.
 The outputs can controlled by buttons from VIS or by setting the object to
 "true" or "false" or "1" or "0".
 
+### Attention
+Please read the pre requirements of adapter
+You have to install by console the following libraries and enable SPI support of Raspberry
+by setting up in "raspi-config"
+
+```
+git clone https://github.com/piface/libmcp23s17.git
+cd libmcp23s17/
+make
+sudo make install
+```
+
+```
+git clone https://github.com/piface/libpifacedigital.git
+cd libpifacedigital/
+make
+sudo make install
+```
+
 To DO:
 - Addressing the Piface to use more than one board
+- Read interval in setup (ms)
+- Selectable invers input (pullup)
 
 ## ChangeLog
+
+### 0.0.20
+* (Eisbaeeer) first beta
 
 ### 0.0.10
 * (Eisbaeeer) initial version
--- piface.js
+++ piface.js
@@ -12,6 +12,9 @@ var utils =   require(__dirname + '/lib/utils'); // Get common adapter utils
 var adapter = utils.adapter('piface');
 var PiFaceIN = [];
 var inp = [];
+var PIFD = require('node-pifacedigital');
+var pi = new PIFD.PIFaceDigital(0,true);
+
 
 adapter.on('ready', function () {
     main();
@@ -23,22 +26,76 @@ adapter.on('objectChange', function (id, obj) {
 
 adapter.on('stateChange', function (id, state) {
     adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+    adapter.log.info('stateVal ' + state.val);
 
     // you can use the ack flag to detect if state is command(false) or status(true)
     if (!state.ack) {
         adapter.log.info('ack is not set!');
-    }
+        
+    // here we go and set the outputs if state of object is changed with no ack
+                if (id == adapter.namespace + '.' + 'OUT0' && state.val == "1" || id == adapter.namespace + '.' + 'OUT0' && state.val == "true" ) {
+                    pi.set(0,1); //(pin,value)
+                    adapter.setState (adapter.namespace + '.' + 'OUT0', {val: true, ack: true});
+                    }
+                if (id == adapter.namespace + '.' + 'OUT0' && state.val == "0" || id == adapter.namespace + '.' + 'OUT0' && state.val == "false" ) {
+                    pi.set(0,0); //(pin,value)
+                    adapter.setState (adapter.namespace + '.' + 'OUT0', {val: false, ack: true});                    
+                    }      
+                if (id == adapter.namespace + '.' + 'OUT1' && state.val == "1" || id == adapter.namespace + '.' + 'OUT1' && state.val == "true" ) {
+                    pi.set(1,1); //(pin,value)
+                    }
+                if (id == adapter.namespace + '.' + 'OUT1' && state.val == "0" || id == adapter.namespace + '.' + 'OUT1' && state.val == "false" ) {
+                    pi.set(1,0); //(pin,value)
+                    }      
+                if (id == adapter.namespace + '.' + 'OUT2' && state.val == "1" || id == adapter.namespace + '.' + 'OUT2' && state.val == "true" ) {
+                    pi.set(2,1); //(pin,value)
+                    }
+                if (id == adapter.namespace + '.' + 'OUT2' && state.val == "0" || id == adapter.namespace + '.' + 'OUT2' && state.val == "false" ) {
+                    pi.set(2,0); //(pin,value)
+                    }      
+                if (id == adapter.namespace + '.' + 'OUT3' && state.val == "1" || id == adapter.namespace + '.' + 'OUT3' && state.val == "true" ) {
+                    pi.set(3,1); //(pin,value)
+                    }
+                if (id == adapter.namespace + '.' + 'OUT3' && state.val == "0" || id == adapter.namespace + '.' + 'OUT3' && state.val == "false" ) {
+                    pi.set(3,0); //(pin,value)
+                    }      
+                if (id == adapter.namespace + '.' + 'OUT4' && state.val == "1" || id == adapter.namespace + '.' + 'OUT4' && state.val == "true" ) {
+                    pi.set(4,1); //(pin,value)
+                    }
+                if (id == adapter.namespace + '.' + 'OUT4' && state.val == "0" || id == adapter.namespace + '.' + 'OUT4' && state.val == "false" ) {
+                    pi.set(4,0); //(pin,value)
+                    }      
+                if (id == adapter.namespace + '.' + 'OUT5' && state.val == "1" || id == adapter.namespace + '.' + 'OUT5' && state.val == "true" ) {
+                    pi.set(5,1); //(pin,value)
+                    }
+                if (id == adapter.namespace + '.' + 'OUT5' && state.val == "0" || id == adapter.namespace + '.' + 'OUT5' && state.val == "false" ) {
+                    pi.set(5,0); //(pin,value)
+                    }      
+                if (id == adapter.namespace + '.' + 'OUT6' && state.val == "1" || id == adapter.namespace + '.' + 'OUT6' && state.val == "true" ) {
+                    pi.set(6,1); //(pin,value)
+                    }
+                if (id == adapter.namespace + '.' + 'OUT6' && state.val == "0" || id == adapter.namespace + '.' + 'OUT6' && state.val == "false" ) {
+                    pi.set(6,0); //(pin,value)
+                    }      
+                if (id == adapter.namespace + '.' + 'OUT7' && state.val == "1" || id == adapter.namespace + '.' + 'OUT07' && state.val == "true" ) {
+                    pi.set(7,1); //(pin,value)
+                    }
+                if (id == adapter.namespace + '.' + 'OUT07' && state.val == "0" || id == adapter.namespace + '.' + 'OUT07' && state.val == "false" ) {
+                    pi.set(7,0); //(pin,value)
+                    }      
+
+  }
 });
 
 
 function pifaceinit(){
     adapter.log.info('function pifaceinit() called!');
+
 }
 
-function pifaceread() {
-    var PIFD = require('node-pifacedigital');
-    var pi = new PIFD.PIFaceDigital(0,true);
-    
+function pifaceread() {  
+var PIFD = require('node-pifacedigital');
+var pi = new PIFD.PIFaceDigital(0,true);  
         PiFaceIN[0] = pi.get(0);
         PiFaceIN[1] = pi.get(1);
         PiFaceIN[2] = pi.get(2);
@@ -139,12 +196,12 @@ function pifaceread() {
                 inp[7] = PiFaceIN[7];
             }
 
-    adapter.log.info('Inputs: ' + PiFaceIN[0] + ":" + PiFaceIN[1] + ":" + PiFaceIN[2] + ":" + PiFaceIN[3] + ":" + PiFaceIN[4] + ":" + PiFaceIN[5] + ":" + PiFaceIN[6] + ":" + PiFaceIN[7] );
+//    adapter.log.info('Inputs: ' + PiFaceIN[0] + ":" + PiFaceIN[1] + ":" + PiFaceIN[2] + ":" + PiFaceIN[3] + ":" + PiFaceIN[4] + ":" + PiFaceIN[5] + ":" + PiFaceIN[6] + ":" + PiFaceIN[7] );
 }
 
 function main() {
     pifaceinit();
-    setInterval(pifaceread, 10000);
+    setInterval(pifaceread, 100);
 //    setInterval(pifaceread, adapter.config.interval * 1000);
     adapter.subscribeStates('*');
             adapter.log.info('function main() called!');
